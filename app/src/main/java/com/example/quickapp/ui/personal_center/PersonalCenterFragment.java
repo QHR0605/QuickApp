67fa0e6512c08fe3cd5c24eca6931f2ab368faf3
QHR0605 <nanku2634>
2021-06-13 22:31:08 +0800

    完成页面设计

--- app/src/main/java/com/example/quickapp/ui/personal_center/PersonalCenterFragment.java
+++ app/src/main/java/com/example/quickapp/ui/personal_center/PersonalCenterFragment.java
@@ -1,13 +1,22 @@
 package com.example.quickapp.ui.personal_center;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.example.quickapp.R;
+import org.jetbrains.annotations.NotNull;
 
 public class PersonalCenterFragment extends Fragment {
 
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
     @Override
-    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_personal_center,container,false);
     }
 }
